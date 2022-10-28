b2ea929008e0e0ef8f603eb5d816418f521e4883
Abbas Sarı <abbassari.sg@gmail.com>
2022-10-28 16:54:49 +0300

    Properties dosyasina site url,mail,isim,sifre eklendi

--- src/test/java/stepDefinitions/AbbasDefinition.java
+++ src/test/java/stepDefinitions/AbbasDefinition.java
@@ -1,4 +1,76 @@
 package stepDefinitions;
 
+import Pages.AbbasPage;
+import io.cucumber.java.en.*;
+
 public class AbbasDefinition {
+
+    AbbasPage abbasPage=new AbbasPage();
+
+    @Given("Belirtilen URL'e gidilir")
+    public void belirtilen_url_e_gidilir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Sing In butonuna tiklanir")
+    public void sing_in_butonuna_tiklanir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("E-mail ve password girisi yapmak icin textbox'larin acildigi dogrulanir")
+    public void e_mail_ve_password_girisi_yapmak_icin_textbox_larin_acildigi_dogrulanir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Acilan pencerede gecerli e-mail ve sifre girildikten sonra Sign In butonuna tiklanir")
+    public void acilan_pencerede_gecerli_e_mail_ve_sifre_girildikten_sonra_sign_in_butonuna_tiklanir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Basariyla giris yapildigi dogrulanir")
+    public void basariyla_giris_yapildigi_dogrulanir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Sayfanin en altinda bulunan My Account'a tiklanir")
+    public void sayfanin_en_altinda_bulunan_my_account_a_tiklanir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Acilan sayfada Store Manager secenegine tiklanir")
+    public void acilan_sayfada_store_manager_secenegine_tiklanir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Store Manager penceresindeyken Products sekmesine tiklanir")
+    public void store_manager_penceresindeyken_products_sekmesine_tiklanir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Sag ust kosede bulunan Add New butonuna tiklanir")
+    public void sag_ust_kosede_bulunan_add_new_butonuna_tiklanir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Acilan sayfanin Add Product sayfasi oldugu dogrulanir")
+    public void acilan_sayfanin_add_product_sayfasi_oldugu_dogrulanir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Acilan sayfada sol altta bulunan listede Shipping kismi tiklanir")
+    public void acilan_sayfada_sol_altta_bulunan_listede_shipping_kismi_tiklanir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Acilan pencerede urunun rengi secilebilmeli birden fazla")
+    public void acilan_pencerede_urunun_rengi_secilebilmeli_birden_fazla() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Acilan pencerede urunun boyutu secilebilmeli birden fazla")
+    public void acilan_pencerede_urunun_boyutu_secilebilmeli_birden_fazla() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
